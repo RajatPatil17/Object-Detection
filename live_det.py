42c5dab9d2ba260f0f17cb7e15dcbc0d30752a98
RajatPatil17 <63973230+RajatPatil17@users.noreply.github.com>
2023-04-28 02:54:15 +0530

    Initial commit

---
+++ live_det.py
@@ -0,0 +1,43 @@
+
+import cv2
+import matplotlib.pyplot as plt
+from IPython.display import clear_output
+thres = 0.5
+url="https://10.19.1.160:8080"
+cap = cv2.VideoCapture(url)
+cap.set(3,1280)     
+cap.set(4,720)
+cap.set(10,70)
+
+classNames= []
+classFile= 'coco.names'
+with open(classFile, 'rt') as f:
+    classNames = f.read().rstrip('\n').split('\n')
+configPath = 'ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt'
+weightsPath = 'frozen_inference_graph.pb'
+
+net = cv2.dnn_DetectionModel(weightsPath, configPath)
+net.setInputSize(320,320)
+net.setInputScale(1.0/127.5)
+net.setInputMean((127.5, 127.5, 127.5))
+net.setInputSwapRB(True)
+
+while True:
+    ret, img = cap.read()
+    classIds, confs, bbox = net.detect(img, confThreshold=thres)
+    print(classIds, bbox)
+
+    if len(classIds) !=0:
+        for classId, confidence, box in zip(classIds.flatten(), confs.flatten(), bbox):
+            cv2.rectangle(img, box, color=(0,255,0), thickness=3)
+            cv2.putText(img, classNames[classId-1].upper(),(box[0]+10, box[1]+30),
+            cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
+    if ret==True:
+        cv2.imshow("Output", img)
+        
+        key = cv2.waitKey(1)
+        if key==ord("q"):
+            break
+cap.release()
+cv2.destroyAllWindows()
+  
