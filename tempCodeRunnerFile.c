b32425caabd6ad67f228381d3535e5af180ff436
RajatPatil17 <63973230+RajatPatil17@users.noreply.github.com>
2023-05-03 16:20:38 +0530

    blue detection

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,17 +1,5 @@
 #include <stdio.h>
-#include <string.h>
 
-int main() {
-  char str1[] = "their", str2[] = "abRd", str3[] = "there";
-  int result;
-
-  // comparing strings str1 and str2
-  result = strcmp("there", "their");
-  printf("strcmp(str1, str2) = %d\n", result);
-
-  // comparing strings str1 and str3
-  result = strcmp(str1, str3);
-  printf("strcmp(str1, str3) = %d\n", result);
-
-  return 0;
+void main() {
+  printf("hell lol");
 }
